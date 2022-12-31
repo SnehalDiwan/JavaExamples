6f1d6223b90c0b97d89289728d05cf7a1fb45f4b
sampa <sampa@Sampada>
2022-12-31 13:10:49 +0530

    added sum method

--- JavaExamples/src/examples/Example1.java
+++ JavaExamples/src/examples/Example1.java
@@ -2,9 +2,14 @@ package examples;
 
 public class Example1 {
 
+	
+	public static int sum(int a , int b) {
+		return a+b;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Example1");
+			System.out.println(sum(4,3));
 	}
 
 }
